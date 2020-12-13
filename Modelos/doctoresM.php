353f35cf3c8589b83c55071de8f9d6282330a1dd
johnnyhicks95 <johnnysomerhald@gmail.com>
2020-12-12 20:35:12 -0500

    desde el perfil de secretarias: se puede crear doctores llenando el modal, se muestra los datos como nombre, usuario, clave , foto, se pueden editar y eliminar los perfiles

---
+++ Modelos/doctoresM.php
@@ -0,0 +1,130 @@
+<?php
+
+require_once "ConexionBD.php";
+
+class DoctoresM extends ConexionBD{
+
+	//Crear Doctores
+	static public function CrearDoctorM($tablaBD, $datosC){
+
+		$pdo = ConexionBD::cBD()->prepare("INSERT INTO $tablaBD(apellido, nombre, sexo, id_consultorio, 
+				usuario, clave, rol) VALUES(:apellido, :nombre, :sexo, :id_consultorio, :usuario, :clave, :rol)");
+
+		$pdo -> bindParam(":apellido", $datosC["apellido"], PDO::PARAM_STR);
+		$pdo -> bindParam(":nombre", $datosC["nombre"], PDO::PARAM_STR);
+		$pdo -> bindParam(":sexo", $datosC["sexo"], PDO::PARAM_STR);
+		$pdo -> bindParam(":id_consultorio", $datosC["id_consultorio"], PDO::PARAM_INT);
+		$pdo -> bindParam(":usuario", $datosC["usuario"], PDO::PARAM_STR);
+		$pdo -> bindParam(":clave", $datosC["clave"], PDO::PARAM_STR);
+		$pdo -> bindParam(":rol", $datosC["rol"], PDO::PARAM_STR);
+
+		if($pdo -> execute()){
+			return true;  // retorna true para la condicion del controlador
+		}
+
+		$pdo -> close();
+		$pdo = null;
+
+	}
+
+
+
+	//Mostrar Doctores
+	static public function VerDoctoresM($tablaBD, $columna, $valor){
+
+		if($columna != null){
+
+			$pdo = ConexionBD::cBD()->prepare("SELECT * FROM $tablaBD WHERE $columna = :$columna");
+
+			$pdo -> bindParam(":".$columna, $valor, PDO::PARAM_STR);
+
+			$pdo->execute();
+
+			return $pdo -> fetchAll();
+
+		}else{
+
+			$pdo = ConexionBD::cBD()->prepare("SELECT * FROM $tablaBD");
+
+			$pdo->execute();
+
+			return $pdo -> fetchAll();
+
+		}
+
+		$pdo -> close();
+		$pdo = null;
+
+	}
+
+
+	//Editar Doctor
+	static public function DoctorM($tablaBD, $columna, $valor){
+
+		if($columna != null){
+
+			$pdo = ConexionBD::cBD()->prepare("SELECT * FROM $tablaBD WHERE $columna = :$columna");
+
+			$pdo -> bindParam(":".$columna, $valor, PDO::PARAM_STR);
+
+			$pdo->execute();
+
+			return $pdo -> fetch();
+
+		}
+
+		$pdo -> close();
+		$pdo = null;
+
+	}
+
+
+
+	//Actualizar Doctores
+	static public function ActualizarDoctorM($tablaBD, $datosC){
+
+		$pdo = ConexionBD::cBD()->prepare("UPDATE $tablaBD SET apellido = :apellido, nombre = :nombre, 
+									sexo = :sexo, usuario = :usuario, clave = :clave WHERE id = :id");
+
+		$pdo -> bindParam(":id", $datosC["id"], PDO::PARAM_INT);
+		$pdo -> bindParam(":apellido", $datosC["apellido"], PDO::PARAM_STR);
+		$pdo -> bindParam(":nombre", $datosC["nombre"], PDO::PARAM_STR);
+		$pdo -> bindParam(":sexo", $datosC["sexo"], PDO::PARAM_STR);
+		$pdo -> bindParam(":usuario", $datosC["usuario"], PDO::PARAM_STR);
+		$pdo -> bindParam(":clave", $datosC["clave"], PDO::PARAM_STR);
+
+		if($pdo -> execute()){
+			return true;
+		}
+
+		$pdo -> close();
+		$pdo = null;
+
+	}
+
+
+
+
+
+	//Eliminar Doctor
+	static public function BorrarDoctorM($tablaBD, $id){
+
+		$pdo = ConexionBD::cBD()->prepare("DELETE FROM $tablaBD WHERE id = :id");
+
+		$pdo -> bindParam(":id", $id, pdo::PARAM_INT);
+
+		if($pdo -> execute()){
+			return true;
+		}
+
+		$pdo -> close();
+		$pdo = null;
+
+	}
+
+
+	
+
+
+
+}

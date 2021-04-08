0aca0108c41d22a1176212330f5b2d51fb7ab6dc
johnnyhicks95 <johnnysomerhald@gmail.com>
2021-04-08 16:55:11 -0500

    agregado las rutas en index y los controladores/modelos en plantilla, creado adminC, adminM, menuAdmin

---
+++ Vistas/modulos/menuAdmin.php
@@ -0,0 +1,46 @@
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+    
+      <ul class="sidebar-menu">
+        
+        <li>
+          <a href="http://localhost/u-clinica/inicio">
+            <i class="fa fa-home"></i>
+            <span>Inicio</span>
+          </a>
+        </li>
+
+        <li>
+          <a href="http://localhost/u-clinica/secretarias">
+            <i class="fa fa-female"></i>
+            <span>Secretarias</span>
+          </a>
+        </li>
+
+        <li>
+          <a href="http://localhost/u-clinica/doctores">
+            <i class="fa fa-user-md"></i>
+            <span>Doctores</span>
+          </a>
+        </li>
+
+        <li>
+          <a href="http://localhost/u-clinica/consultorios">
+            <i class="fa fa-medkit"></i>
+            <span>Consultorios</span>
+          </a>
+        </li>
+
+        <li>
+          <a href="http://localhost/u-clinica/pacientes">
+            <i class="fa fa-users"></i>
+            <span>Pacientes</span>
+          </a>
+        </li>
+
+      </ul>
+
+    </section>
+    <!-- /.sidebar -->
+  </aside>

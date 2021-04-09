2609acee30fa18a73d57a1225802d5f0d49e7d3f
johnnyhicks95 <johnnysomerhald@gmail.com>
2021-04-08 20:08:43 -0500

    creados: inicioM, inicio-editar, inicioC. Solo el administrador puede crear, editar el contenido de inicio. Agregado el favicon que se pueda cambiar

--- Vistas/plantilla.php
+++ Vistas/plantilla.php
@@ -13,6 +13,12 @@ session_start();
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
+  <?php 
+    $favicon = new InicioC();
+    $favicon -> FaviconC();
+  ?>
+  <link rel="icon" type="" href="Vistas/img/favicon.png" >
+
   <!-- Bootstrap 3.3.7 -->
   <link rel="stylesheet" href="http://localhost/u-clinica/Vistas/bower_components/bootstrap/dist/css/bootstrap.min.css">
   <!-- Font Awesome -->
